c06a68c76616ca5ed1ba27db21bbc11a8b109370
Rishabh Goel <rishu@Rishus-MacBook-Pro.local>
2019-01-19 13:41:30 +0530

    ProductMicroService Setup

---
+++ src/main/java/com/example/ProductsMicroService/ProductsMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.ProductsMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMicroServiceApplication.class, args);
+	}
+
+}
+
