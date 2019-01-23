8eb237ef8bb0f19aaaa4c41c51f783468dd3c623
Rishabh Goel <rishu@Rishus-MacBook-Pro.local>
2019-01-23 16:07:12 +0530

    Swagger Integration Done

--- src/main/java/com/example/ProductsMicroService/SwaggerConfig.java
+++ src/main/java/com/example/ProductsMicroService/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.example.ProductsMicroService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 import java.util.regex.*;
 
 import static springfox.documentation.builders.PathSelectors.regex;
@@ -17,8 +22,9 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.example.ProductsMicroService"))
+                .select().apis(RequestHandlerSelectors.basePackage("com.example.ProductsMicroService.Controller"))
                 .paths(regex("/products.*"))
                 .build();
     }
+
 }
